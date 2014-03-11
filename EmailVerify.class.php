6a662585356a1d8484de3489b7ba0f148d427641
DukeOfMarshall <DukeOfMarshall@gmail.com>
2014-03-11 11:56:58 -0400

    Update EmailVerify.class.php

--- EmailVerify.class.php
+++ EmailVerify.class.php
@@ -22,30 +22,32 @@ if(basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])){
 	$return_array = array();
 	
 	if($_GET['address_to_verify'] == '' || !isset($_GET['address_to_verify'])){
-		$return_array['error'] 			= 1;
-		$return_array['message'] 		= 'No email address was submitted for verification';
+		$return_array['error'] 				= 1;
+		$return_array['message'] 			= 'No email address was submitted for verification';
 		$return_array['domain_verified'] 	= 0;
 		$return_array['format_verified'] 	= 0;
 	}else{
 		$verify = new EmailVerify();
 		
-		if($verify->verify_formatting($_GET['address_to_verify'])){
-			$return_array['format_verified'] = 1;
+		$format_check = $verify->verify_formatting($_GET['address_to_verify'], $_GET['verbose']);
+		
+		if($format_check === TRUE){
+			$return_array['format_verified'] 	= 1;
 			
 			if($verify->verify_domain($_GET['address_to_verify'])){
-				$return_array['error'] 			= 0;
+				$return_array['error'] 				= 0;
 				$return_array['domain_verified'] 	= 1;
-				$return_array['message'] 		= 'Formatting and domain have been verified';
+				$return_array['message'] 			= 'Formatting and domain have been verified';
 			}else{
-				$return_array['error'] 			= 1;
+				$return_array['error'] 				= 1;
 				$return_array['domain_verified'] 	= 0;
-				$return_array['message'] 		= 'Formatting was verified, but verification of the domain has failed';
+				$return_array['message'] 			= 'Formatting was verified, but verification of the domain has failed';
 			}
 		}else{
-			$return_array['error'] 			= 1;
+			$return_array['error'] 				= 1;
 			$return_array['domain_verified'] 	= 0;
 			$return_array['format_verified'] 	= 0;
-			$return_array['message'] 		= 'Email was not formatted correctly';
+			$return_array['message'] 			= $format_check ?: 'Email was not formatted correctly';
 		}
 	}
 	
@@ -55,22 +57,455 @@ if(basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])){
 }
 
 class EmailVerify {
+	# Comprehensive list of domain name extensions from http://data.iana.org/TLD/tlds-alpha-by-domain.txt
+	# http://www.icann.org/en/resources/registries/tlds
+	#
+	# Does not include domains with non-latin characters
+	public $list_domain_extensions = array(
+										'ac',
+										'academy',
+										'actor',
+										'ad',
+										'ae',
+										'aero',
+										'af',
+										'ag',
+										'agency',
+										'ai',
+										'al',
+										'am',
+										'an',
+										'ao',
+										'aq',
+										'ar',
+										'arpa',
+										'as',
+										'asia',
+										'at',
+										'au',
+										'aw',
+										'ax',
+										'az',
+										'ba',
+										'bar',
+										'bargains',
+										'bb',
+										'bd',
+										'be',
+										'berlin',
+										'best',
+										'bf',
+										'bg',
+										'bh',
+										'bi',
+										'bid',
+										'bike',
+										'biz',
+										'bj',
+										'blue',
+										'bm',
+										'bn',
+										'bo',
+										'boutique',
+										'br',
+										'bs',
+										'bt',
+										'build',
+										'builders',
+										'buzz',
+										'bv',
+										'bw',
+										'by',
+										'bz',
+										'ca',
+										'cab',
+										'camera',
+										'camp',
+										'cards',
+										'careers',
+										'cat',
+										'catering',
+										'cc',
+										'cd',
+										'center',
+										'ceo',
+										'cf',
+										'cg',
+										'ch',
+										'cheap',
+										'christmas',
+										'ci',
+										'ck',
+										'cl',
+										'cleaning',
+										'clothing',
+										'club',
+										'cm',
+										'cn',
+										'co',
+										'codes',
+										'coffee',
+										'com',
+										'community',
+										'company',
+										'computer',
+										'condos',
+										'construction',
+										'contractors',
+										'cool',
+										'coop',
+										'cr',
+										'cruises',
+										'cu',
+										'cv',
+										'cw',
+										'cx',
+										'cy',
+										'cz',
+										'dance',
+										'dating',
+										'de',
+										'democrat',
+										'diamonds',
+										'directory',
+										'dj',
+										'dk',
+										'dm',
+										'do',
+										'domains',
+										'dz',
+										'ec',
+										'edu',
+										'education',
+										'ee',
+										'eg',
+										'email',
+										'enterprises',
+										'equipment',
+										'er',
+										'es',
+										'estate',
+										'et',
+										'eu',
+										'events',
+										'expert',
+										'exposed',
+										'farm',
+										'fi',
+										'fish',
+										'fj',
+										'fk',
+										'flights',
+										'florist',
+										'fm',
+										'fo',
+										'foundation',
+										'fr',
+										'futbol',
+										'ga',
+										'gallery',
+										'gb',
+										'gd',
+										'ge',
+										'gf',
+										'gg',
+										'gh',
+										'gi',
+										'gift',
+										'gl',
+										'glass',
+										'gm',
+										'gn',
+										'gov',
+										'gp',
+										'gq',
+										'gr',
+										'graphics',
+										'gs',
+										'gt',
+										'gu',
+										'guitars',
+										'guru',
+										'gw',
+										'gy',
+										'hk',
+										'hm',
+										'hn',
+										'holdings',
+										'holiday',
+										'house',
+										'hr',
+										'ht',
+										'hu',
+										'id',
+										'ie',
+										'il',
+										'im',
+										'immobilien',
+										'in',
+										'industries',
+										'info',
+										'institute',
+										'int',
+										'international',
+										'io',
+										'iq',
+										'ir',
+										'is',
+										'it',
+										'je',
+										'jm',
+										'jo',
+										'jobs',
+										'jp',
+										'kaufen',
+										'ke',
+										'kg',
+										'kh',
+										'ki',
+										'kim',
+										'kitchen',
+										'kiwi',
+										'km',
+										'kn',
+										'koeln',
+										'kp',
+										'kr',
+										'kred',
+										'kw',
+										'ky',
+										'kz',
+										'la',
+										'land',
+										'lb',
+										'lc',
+										'li',
+										'lighting',
+										'limo',
+										'link',
+										'lk',
+										'lr',
+										'ls',
+										'lt',
+										'lu',
+										'luxury',
+										'lv',
+										'ly',
+										'ma',
+										'maison',
+										'management',
+										'mango',
+										'marketing',
+										'mc',
+										'md',
+										'me',
+										'menu',
+										'mg',
+										'mh',
+										'mil',
+										'mk',
+										'ml',
+										'mm',
+										'mn',
+										'mo',
+										'mobi',
+										'moda',
+										'monash',
+										'mp',
+										'mq',
+										'mr',
+										'ms',
+										'mt',
+										'mu',
+										'museum',
+										'mv',
+										'mw',
+										'mx',
+										'my',
+										'mz',
+										'na',
+										'nagoya',
+										'name',
+										'nc',
+										'ne',
+										'net',
+										'neustar',
+										'nf',
+										'ng',
+										'ni',
+										'ninja',
+										'nl',
+										'no',
+										'np',
+										'nr',
+										'nu',
+										'nz',
+										'okinawa',
+										'om',
+										'onl',
+										'org',
+										'pa',
+										'partners',
+										'parts',
+										'pe',
+										'pf',
+										'pg',
+										'ph',
+										'photo',
+										'photography',
+										'photos',
+										'pics',
+										'pink',
+										'pk',
+										'pl',
+										'plumbing',
+										'pm',
+										'pn',
+										'post',
+										'pr',
+										'pro',
+										'productions',
+										'properties',
+										'ps',
+										'pt',
+										'pub',
+										'pw',
+										'py',
+										'qa',
+										'qpon',
+										're',
+										'recipes',
+										'red',
+										'rentals',
+										'repair',
+										'report',
+										'reviews',
+										'rich',
+										'ro',
+										'rs',
+										'ru',
+										'ruhr',
+										'rw',
+										'sa',
+										'sb',
+										'sc',
+										'sd',
+										'se',
+										'sexy',
+										'sg',
+										'sh',
+										'shiksha',
+										'shoes',
+										'si',
+										'singles',
+										'sj',
+										'sk',
+										'sl',
+										'sm',
+										'sn',
+										'so',
+										'social',
+										'solar',
+										'solutions',
+										'sr',
+										'st',
+										'su',
+										'supplies',
+										'supply',
+										'support',
+										'sv',
+										'sx',
+										'sy',
+										'systems',
+										'sz',
+										'tattoo',
+										'tc',
+										'td',
+										'technology',
+										'tel',
+										'tf',
+										'tg',
+										'th',
+										'tienda',
+										'tips',
+										'tj',
+										'tk',
+										'tl',
+										'tm',
+										'tn',
+										'to',
+										'today',
+										'tokyo',
+										'tools',
+										'tp',
+										'tr',
+										'training',
+										'travel',
+										'tt',
+										'tv',
+										'tw',
+										'tz',
+										'ua',
+										'ug',
+										'uk',
+										'uno',
+										'us',
+										'uy',
+										'uz',
+										'va',
+										'vacations',
+										'vc',
+										've',
+										'ventures',
+										'vg',
+										'vi',
+										'viajes',
+										'villas',
+										'vision',
+										'vn',
+										'vote',
+										'voting',
+										'voto',
+										'voyage',
+										'vu',
+										'wang',
+										'watch',
+										'wed',
+										'wf',
+										'wien',
+										'wiki',
+										'works',
+										'ws',
+										'xxx',
+										'xyz',
+										'ye',
+										'yt',
+										'za',
+										'zm',
+										'zone',
+										'zw'
+	);
+	
 	public function __construct(){
 	}
 	
 	# Verify the DNS records according to the domain name given in the email address
-	public function verify_domain($address_to_verify){
+	public function verify_domain($address_to_verify, $verbose=FALSE){
 		$record = 'MX'; # <-- Can be changed to check for other records like A records or CNAME records as well
 		list($user, $domain) = explode('@', $address_to_verify);
 		return checkdnsrr($domain, $record);
 	}
 	
 	# Verify that the email address is formatted as an email address should be
-	public function verify_formatting($address_to_verify){
+	public function verify_formatting($address_to_verify, $verbose=FALSE){
 		
 		# Check to make sure the @ symbol is included
 		if(strstr($address_to_verify, "@") == FALSE){
-			return false;
+			if($verbose){
+				return 'Ampersand not present.';
+			}else{
+				return false;
+			}
 		}else{
 			
 			# Bust up the address so that we have the name and the domain name
@@ -78,9 +513,34 @@ class EmailVerify {
 			
 			# Verify the domain name has a period like all good domain names should
 			if(strstr($domain, '.') == FALSE){
-				return false;
+				if($verbose){
+					return 'Period not present.';
+				}else{
+					return false;
+				}
 			}else{
-				return true;
+				
+				# Bust up the domain name
+				$domain_check = explode(".", $domain);
+				$domain_extension = end($domain_check);
+				
+				if(strlen($domain_extension) < 2){
+					if($verbose){
+						return 'Domain name extension is too short.';
+					}else{
+						return false;
+					}
+				}else{
+					if(!in_array($domain_extension, $this->list_domain_extensions)){
+						if($verbose){
+							return 'Domain name extension could not be verified.';
+						}else{
+							return false;
+						}
+					}else{
+						return true;
+					}
+				}
 			}
 		}
 	}
